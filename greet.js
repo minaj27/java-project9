4cb281f99afb8a3466543c8ce9da555702cec924
minaj27 <minaj13m@gmail.com>
2023-12-04 23:53:53 +0530

    first commit

---
+++ greet.js
@@ -0,0 +1,5 @@
+function greet(msg ,clr){
+    document.getElementById("hmsg").innerHTML =msg;
+    document.getElementById("bg").style.backgroundColor=clr;
+    document.getElementById("mmsg").innerHTML =msg;
+}
